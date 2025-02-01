ddc389d8b4bca51bd47c8a05991ffddc235d888e
Drew Gallagher <drewg2009@gmail.com>
2025-01-31 23:20:40 -0400

    added frontend client to interface with api
    added middleware to decode encoded inputString to support inputStrings that have special characters

--- index.js
+++ index.js
@@ -2,8 +2,32 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+/**
+ * Global request middlware
+ * 
+ * Handles decoding of input strings that are using encodeURIComponent on the client side
+ * This way we can process special character decoding per request in one spot
+ */
+app.use((req, res, next) => {
+  if (req.path.includes('/api')) {
+    if (req.query.inputString) {
+      const decodedInputString = getDecodedInputString(req.query.inputString)
+      if (decodedInputString === decodeErrorMessage) {
+        return getDecodedInputStringErrResponse()
+      }
+    }
+  }
+  next()
+})
 // Middleware to parse JSON request bodies
 app.use(express.json());
+app.set('view engine', 'pug')
+const router = express.Router()
+//a middleware sub-stack shows request info for any type of HTTP request to the /user/:id path
+
+
+const decodeErrorMessage = 'Invalid input string. Could not decode URI component.'
+
 
 // Function to remove all non-numeric characters
 function onlyNumbers(str) {
@@ -19,19 +43,27 @@ function onlySpecialCharacters(str) {
   return str.replace(/[a-zA-Z0-9\s]/g, ''); // Keep only special characters
 }
 
+function getDecodedInputString(inputString) {
+
+  // Decode the URI component to handle special characters
+  try {
+    inputString = decodeURIComponent(inputString);
+  } catch (error) {
+    return decodeErrorMessage
+  }
+  return inputString
+}
+
+function getDecodedInputStringErrResponse() {
+  return res.status(400).json({ error: decodeErrorMessage })
+}
+
 // GET route for onlySpecialCharacters
 app.get('/api/onlySpecialCharacters', (req, res) => {
   let inputString = req.query.inputString;
 
   if (!inputString) {
     return res.status(400).json({ error: 'Input string is required as a query parameter.' });
-  }
-
-    // Decode the URI component to handle special characters
-  try {
-    inputString = decodeURIComponent(inputString);
-  } catch (error) {
-    return res.status(400).json({ error: 'Invalid input string. Could not decode URI component.' });
   }
 
   const result = onlySpecialCharacters(inputString);
@@ -41,27 +73,30 @@ app.get('/api/onlySpecialCharacters', (req, res) => {
 // Example using query parameters (GET requests)
 
 app.get('/api/onlyNumbers', (req, res) => {
-    const inputString = req.query.inputString;
-    console.log(req.query)
-    if (!inputString) {
-        return res.status(400).json({ error: 'Input string is required as a query parameter.' });
-      }
-    
-      const result = onlyNumbers(inputString);
-      res.json({ result });
+  const inputString = req.query.inputString;
+  console.log(req.query)
+  if (!inputString) {
+    return res.status(400).json({ error: 'Input string is required as a query parameter.' });
+  }
+
+  const result = onlyNumbers(inputString);
+  res.json({ result });
 });
 
 app.get('/api/onlyLetters', (req, res) => {
-    const inputString = req.query.inputString;
+  const inputString = req.query.inputString;
 
-    if (!inputString) {
-        return res.status(400).json({ error: 'Input string is required as a query parameter.' });
-      }
-    
-      const result = onlyLetters(inputString);
-      res.json({ result });
+  if (!inputString) {
+    return res.status(400).json({ error: 'Input string is required as a query parameter.' });
+  }
+
+  const result = onlyLetters(inputString);
+  res.json({ result });
 });
 
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
---
+++ views/script.js
@@ -0,0 +1,25 @@
+async function getResponse() {
+    const endpoint = document.querySelector('select[name="endpoint"]').value
+    const inputString = document.querySelector('#inputString').value
+    const encodedInputString = encodeURIComponent(inputString)
+
+    const baseUrl = 'http://localhost:3000/api/'
+    // const baseUrl = 'https://readable-regex-8d81b79167bf.herokuapp.com/api/'
+
+    try {
+        const response = await fetch(baseUrl + endpoint + "?inputString=" + encodedInputString)
+        const json = await response.json()
+        const transformedString = json.result
+        document.querySelector('#responseBox').textContent = transformedString
+
+    }
+    catch(exception) {
+        alert('Error executing regex, try again later! Contact developer for support')
+        throw exception
+    }
+}
+
+function enableButton() {
+    const inputString = document.querySelector('#inputString').value
+    document.querySelector('#getResponseButton').disabled = inputString.length > 0 ? false : true
+}
