d60770f9b667ef28912a2b9fa6c6864f7f7f509b
Drew Gallagher <drewg2009@gmail.com>
2025-02-01 23:49:26 -0400

    added cors to allow localhost on server side to hit api url

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors')
 
+
+app.use('cors')
 /**
  * Global request middlware
  * 
