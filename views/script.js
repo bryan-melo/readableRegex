02fe6e4a73a4f1d70beb22a44764b45542b1a7be
Drew Gallagher <drewg2009@gmail.com>
2025-02-05 20:04:43 -0400

    removed decoding/encoding since it's no longer needed when using POST body
    updated examples/docs

--- views/script.js
+++ views/script.js
@@ -1,20 +1,18 @@
 async function getResponse() {
     const endpoint = document.querySelector('select[name="endpoint"]').value
     const inputString = document.querySelector('#inputString').value
-    const encodedInputString = encodeURIComponent(inputString)
-
 
     //TODO add env variables to dynamically load the right url
     // local dev  url: DO NOT COMMIT THIS TO PROD
-    // const baseUrl = 'http://localhost:3000/api/'
+    const baseUrl = 'http://localhost:3000/api/'
     // prod url
-    const baseUrl = 'https://readable-regex-8d81b79167bf.herokuapp.com/api/'
+    // const baseUrl = 'https://readable-regex-8d81b79167bf.herokuapp.com/api/'
 
     try {
         const response = await fetch(baseUrl + endpoint, {
             method: 'POST',
             body: JSON.stringify({
-                inputString: encodedInputString
+                inputString: inputString
             }),
             headers: {
                 "Content-Type": "application/json"
