89001665e81924cae1b84d53789696ab594d81ea
Rahul Shah <rahulnshah3012@gmail.com>
2025-02-06 22:34:23 -0500

    moved tests for ValidationFunctions.onlyTheseCharacters to a different file

--- test/onlyNumbers.test.js
+++ test/onlyNumbers.test.js
@@ -24,12 +24,4 @@ describe('ValidationFunctions.onlyNumbers', () => {
     expect(ValidationFunctions.onlyNumbers('Price: $12.99')).toBe('1299');
     expect(ValidationFunctions.onlyNumbers('ID#123-ABC-456')).toBe('123456');
   });
-
-  it("should only include certain characters", () => {
-    expect(ValidationFunctions.includeOnlyTheseCharacters("$drew$%", ["$", "%"])).toBe("$$%");
-    expect(ValidationFunctions.includeOnlyTheseCharacters("123abc456", ["1", "2", "3"])).toBe("123");
-    expect(ValidationFunctions.includeOnlyTheseCharacters("abc123def456ghi789", ["7", "8", "9"])).toBe("789");
-    expect(ValidationFunctions.includeOnlyTheseCharacters("!@#123$%^456&*()", ["4", "5", "6"])).toBe("456");
-    expect(ValidationFunctions.includeOnlyTheseCharacters("abc", ["1", "2", "3"])).toBe("");
-  });
 });
